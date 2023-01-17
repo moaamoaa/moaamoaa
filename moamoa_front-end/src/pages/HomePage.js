4f2ea047b4a16439f21be04601ac420914b7b536
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-01-17 14:55:17 +0900

    🗑️ 주석 삭제

---
+++ moamoa_front-end/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import Container from '@mui/material/Container';
+import Navbar from '../components/Navbar';
+
+function App() {
+  return (
+    <Container fixed>
+      <Navbar></Navbar>
+    </Container>
+  );
+}
