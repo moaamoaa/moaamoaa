ea964daaeb505133f7417a2dcb2ab4bb0dc04858
jiyeonyou0416 <jiyoenyou0416@naver.com>
2023-01-18 17:43:42 +0900

    :hammer: update userservice

--- moamoa_back-end/src/main/java/com/ssafy/moamoa/controller/UserController.java
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.ssafy.moamoa.controller;
 
-import java.util.HashMap;
-import java.util.List;
-
-import javax.transaction.Transactional;
-import javax.validation.constraints.Email;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ssafy.moamoa.domain.User;
+import com.ssafy.moamoa.dto.SignUpForm;
+import com.ssafy.moamoa.service.ProfileService;
+import com.ssafy.moamoa.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,21 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.ssafy.moamoa.domain.Profile;
-import com.ssafy.moamoa.domain.User;
-import com.ssafy.moamoa.dto.SignUpForm;
-import com.ssafy.moamoa.service.ProfileService;
-import com.ssafy.moamoa.service.UserService;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -36,23 +25,24 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 public class UserController {
 
-	private final UserService userService;
-	private final ProfileService profileService;
-	@GetMapping
-	public ResponseEntity<?> showList() throws Exception {
-		List<User> users = userService.findUsers();
-		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
-	}
+    private final UserService userService;
+    private final ProfileService profileService;
 
-	@PostMapping("/signup")
-	public ResponseEntity<?> signup(@RequestBody SignUpForm signUpForm) throws JsonProcessingException {
+    @GetMapping
+    public ResponseEntity<?> showList() throws Exception {
+        List<User> users = userService.findUsers();
+        return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+    }
 
-		String email = signUpForm.getEmail();
-		String password = signUpForm.getPassword();
-		String nickname = signUpForm.getNickname();
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody SignUpForm signUpForm) throws JsonProcessingException {
 
-		String userNickname = userService.signup(email, password, nickname);
+        String email = signUpForm.getEmail();
+        String password = signUpForm.getPassword();
+        String nickname = signUpForm.getNickname();
 
-		return new ResponseEntity<String>(userNickname, HttpStatus.OK);
-	}
+        String userNickname = userService.signup(email, password, nickname);
+
+        return new ResponseEntity<String>(userNickname, HttpStatus.OK);
+    }
 }
--- moamoa_back-end/src/main/java/com/ssafy/moamoa/service/UserService.java
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/service/UserService.java
@@ -1,75 +1,65 @@
 package com.ssafy.moamoa.service;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.ssafy.moamoa.domain.Profile;
 import com.ssafy.moamoa.domain.User;
 import com.ssafy.moamoa.exception.DuplicateProfileNicknameException;
 import com.ssafy.moamoa.exception.DuplicateUserEmailException;
 import com.ssafy.moamoa.repository.ProfileRepository;
 import com.ssafy.moamoa.repository.UserRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
-	private final UserRepository userRepository;
-	private final ProfileRepository profileRepository;
+    private final UserRepository userRepository;
+    private final ProfileRepository profileRepository;
 
-	// 회원 전체 조회
-	public List<User> findUsers() {
-		return userRepository.findAll();
-	}
+    // 회원 전체 조회
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
 
-	// 회원 가입 - user
-	public User save(User user) {
-		validateDuplicateUserEmail(user); // 중복 이메일 검증
-		return userRepository.save(user);
-	}
+    // 이메일 중복 조회
+    private void validateDuplicateUserEmail(User user) {
+        List<User> findUsers = userRepository.findByEmail(user.getEmail());
+        if (!findUsers.isEmpty()) {
+            throw new DuplicateUserEmailException("이미 존재하는 회원입니다.");
+        }
+    }
 
-	// 이메일 중복 조회
-	private void validateDuplicateUserEmail(User user) {
-		List<User> findUsers = userRepository.findByEmail(user.getEmail());
-		if (!findUsers.isEmpty()) {
-			throw new DuplicateUserEmailException("이미 존재하는 회원입니다.");
-		}
-	}
+    // 닉네임 중복 조회
+    private void validateDuplicateProfileNickname(Profile profile) {
+        List<Profile> findProfiles = profileRepository.findByNickname(profile.getNickname());
+        if (!findProfiles.isEmpty()) {
+            throw new DuplicateProfileNicknameException("이미 존재하는 닉네임입니다.");
+        }
+    }
 
-	// 닉네임 중복 조회
-	private void validateDuplicateProfileNickname(Profile profile) {
-		List<Profile> findProfiles = profileRepository.findByNickname(profile.getNickname());
-		if (!findProfiles.isEmpty()) {
-			throw new DuplicateProfileNicknameException("이미 존재하는 닉네임입니다.");
-		}
-	}
+    // 회원 가입
+    public String signup(String email, String password, String nickname) {
+        // user
+        User user = User.builder()
+                .email(email)
+                .password(password)
+                .build();
+        // profile
+        Profile profile = Profile.builder()
+                .nickname(nickname)
+                .build();
 
-	// 회원 가입
-	public String signup(String email, String password, String nickname) {
-		// user
-		User user = User.builder()
-					.email(email)
-					.password(password)
-					.build();
-		// profile
-		Profile profile = Profile.builder()
-								.nickname(nickname)
-								.build();
+        user.setProfile(profile);
 
-		user.setProfile(profile);
+        validateDuplicateUserEmail(user);
+        validateDuplicateProfileNickname(profile);
 
-		validateDuplicateUserEmail(user);
-		validateDuplicateProfileNickname(profile);
-
-		userRepository.save(user);
-		profileRepository.save(profile);
-		return nickname;
-	}
+        userRepository.save(user);
+        profileRepository.save(profile);
+        return nickname;
+    }
 }
