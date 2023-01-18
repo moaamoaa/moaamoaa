e607ffb75cb35a77193326665f0351317d97caa9
jyuha <jyuha759@gmail.com>
2023-01-18 15:26:07 +0900

    :art: Add CardList and CardItem and downgrade react version

---
+++ moamoa_front-end/src/components/CardList.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CardItem from './CardItem';
+
+// 표시할 카드 개수 지정
+const cards = [1, 2, 3];
+
+const theme = createTheme();
+
+export default function ListPage() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            금주의 추천 프로젝트
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <CardItem>
+                  CardItem컴포넌트 props 대신 redux 이용해서 넣을 부분
+                </CardItem>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
+  );
+}
