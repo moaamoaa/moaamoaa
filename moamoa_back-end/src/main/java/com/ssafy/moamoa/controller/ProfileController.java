cf74cbfa7a2e6345390bec3fb74ff3c11b79f8b8
smartpodo@kakao.com <smartpodo@kakao.com>
2023-01-19 16:56:40 +0900

    :bug: Fixed Conflict at UserController

--- moamoa_back-end/src/main/java/com/ssafy/moamoa/controller/ProfileController.java
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/controller/ProfileController.java
@@ -5,14 +5,19 @@ import java.util.Map;
 
 import javax.transaction.Transactional;
 
-import com.ssafy.moamoa.dto.ProfileForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.moamoa.domain.Profile;
-import com.ssafy.moamoa.service.ProfileServiceImpl;
+import com.ssafy.moamoa.dto.ProfileForm;
+import com.ssafy.moamoa.dto.UserForm;
+import com.ssafy.moamoa.service.ProfileService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,21 +31,27 @@ public class ProfileController {
 	private static final String SUCCESS = "SUCCESS";
 	private static final String FAIL = "FAIL";
 
-	private ProfileServiceImpl profileService;
+	private ProfileService profileService;
 
 	@Autowired
-	public ProfileController(ProfileServiceImpl profileService){
+	public ProfileController(ProfileService profileService) {
 		this.profileService = profileService;
 	}
 
 	// 검색 여부 변경
 	@PutMapping("/searchState")
-	ResponseEntity<Map<String, Object>> updateSearchState(@RequestBody Long userId) {
+	// input userId output String ProfileSearchStatus
+	ResponseEntity<Map<String, Object>> updateSearchState(@RequestBody UserForm user) {
 		Map<String, Object> resultMap = new HashMap<>();
 		HttpStatus status = null;
-		ProfileForm profileForm = profileService.changeUserState(userId);
-		resultMap.put("message",profileForm.getProfileSearchStatus());
-		status= HttpStatus.ACCEPTED;
+
+		log.info("userId log={}", user.getId());
+
+		ProfileForm profileForm = profileService.changeUserState(user.getId());
+		resultMap.put("message", profileForm.getProfileSearchStatus());
+
+		status = HttpStatus.ACCEPTED;
+		resultMap.put("status", status);
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
 
@@ -54,7 +65,7 @@ public class ProfileController {
 	}
 
 	// 다른 사용자 페이지 접근
-	@GetMapping("/user")
+	@GetMapping("/user/{userId}")
 	public ResponseEntity<Map<String, Object>> goUserPage(@PathVariable String userId) {
 		Map<String, Object> resultMap = new HashMap<>();
 		HttpStatus status = null;
--- moamoa_back-end/src/main/java/com/ssafy/moamoa/controller/UserController.java
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.ssafy.moamoa.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.ssafy.moamoa.domain.Profile;
-import com.ssafy.moamoa.domain.User;
-import com.ssafy.moamoa.dto.SignUpForm;
-import com.ssafy.moamoa.service.ProfileServiceImpl;
-import com.ssafy.moamoa.service.UserService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.transaction.Transactional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,8 +13,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.transaction.Transactional;
-import java.util.List;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ssafy.moamoa.domain.Profile;
+import com.ssafy.moamoa.domain.User;
+import com.ssafy.moamoa.dto.Mail;
+import com.ssafy.moamoa.dto.SignUpForm;
+import com.ssafy.moamoa.service.MailService;
+import com.ssafy.moamoa.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
@@ -26,44 +31,55 @@ import java.util.List;
 @Transactional
 public class UserController {
 
-    private final UserService userService;
-    private final ProfileServiceImpl profileService;
+	private final UserService userService;
+	private final MailService mailService;
 
-    @GetMapping
-    public ResponseEntity<?> showList() throws Exception {
-        List<User> users = userService.findUsers();
-        return new ResponseEntity<List<User>>(users, HttpStatus.OK);
-    }
+	@GetMapping
+	public ResponseEntity<?> showList() throws Exception {
+		List<User> users = userService.findUsers();
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+	}
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody SignUpForm signUpForm) throws JsonProcessingException {
+	// 회원 가입
+	@PostMapping("/signup")
+	public ResponseEntity<?> signup(@RequestBody SignUpForm signUpForm) throws JsonProcessingException {
 
-        String email = signUpForm.getEmail();
-        String password = signUpForm.getPassword();
-        String nickname = signUpForm.getNickname();
+		String email = signUpForm.getEmail();
+		String password = signUpForm.getPassword();
+		String nickname = signUpForm.getNickname();
 
-        String userNickname = userService.signup(email, password, nickname);
+		String userNickname = userService.signup(email, password, nickname);
 
-        return new ResponseEntity<String>(userNickname, HttpStatus.OK);
-    }
+		return new ResponseEntity<String>(userNickname, HttpStatus.OK);
+	}
 
-    @PostMapping("/checkemail")
-    public ResponseEntity<?> checkEmail(@RequestBody String email) throws JsonProcessingException {
-        System.out.println(email);
-        User user = User.builder()
-                .email(email)
-                .build();
-        userService.validateDuplicateUserEmail(user);
+	// 회원 가입 시 메일 유효성 검사
+	@PostMapping("/mail")
+	public ResponseEntity<?> signupMail(@RequestBody Mail mail) throws MessagingException {
+		String code = mailService.joinEmail(mail.getMailTo());
 
-        return new ResponseEntity<String>("이메일 중복 확인", HttpStatus.OK);
-    }
+		return new ResponseEntity<String>(code, HttpStatus.OK);
+	}
 
-    @PostMapping("/checkenickname")
-    public ResponseEntity<?> checkNickName(@RequestBody String nickname) throws JsonProcessingException {
-        Profile profile = Profile.builder()
-                .nickname(nickname)
-                .build();
-        userService.validateDuplicateProfileNickname(profile);
-        return new ResponseEntity<String>("닉네임 중복 확인", HttpStatus.OK);
-    }
+	@PostMapping("/email")
+	public ResponseEntity<?> checkEmail(@RequestBody String email) throws JsonProcessingException {
+		System.out.println(email);
+		User user = User.builder()
+			.email(email)
+			.build();
+		userService.validateDuplicateUserEmail(user);
+
+		return new ResponseEntity<String>("이메일 중복 확인", HttpStatus.OK);
+	}
+
+	@PostMapping("/nickname")
+	public ResponseEntity<?> checkNickName(@RequestBody String nickname) throws JsonProcessingException {
+		Profile profile = Profile.builder()
+			.nickname(nickname)
+			.build();
+		userService.validateDuplicateProfileNickname(profile);
+		return new ResponseEntity<String>("닉네임 중복 확인", HttpStatus.OK);
+
+	}
+
 }
