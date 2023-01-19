70cfc7ff6957efd11b7c553917148bb20fe72641
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-01-19 14:10:28 +0900

    📝 Rename Dialog files

---
+++ moamoa_front-end/src/components/SignUpEmailForm.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import DialogContent from '@mui/material/DialogContent';
+import Typography from '@mui/material/Typography';
+
+export default function SignUpEmailDialog() {
+  return (
+    <DialogContent dividers>
+      <Typography gutterBottom>모아모아에 오신 것을 환영합니다.</Typography>
+      <Typography gutterBottom>이메일을 알려주세요!</Typography>
+      <TextField
+        autoFocus
+        margin="dense"
+        id="name"
+        label="Email Address"
+        type="email"
+        fullWidth
+        variant="standard"
+      />
+    </DialogContent>
+  );
+}
