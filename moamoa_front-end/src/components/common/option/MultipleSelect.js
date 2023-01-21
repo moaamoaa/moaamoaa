6ef654bb9c86f7ce621aae509dc7674c11047f2b
jyuha <jyuha759@gmail.com>
2023-01-21 22:00:03 +0900

    :art: Add Select

---
+++ moamoa_front-end/src/components/common/option/MultipleSelect.js
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const teches = [
+  'Javascript',
+  'Typescript',
+  'React',
+  'Vue',
+  'Angular',
+  'Svelte',
+];
+
+function getStyles(tech, personTech, theme) {
+  return {
+    fontWeight:
+      personTech.indexOf(tech) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+export default function MultipleSelectChip() {
+  const theme = useTheme();
+  const [personTech, setPersonTech] = React.useState([]);
+
+  const handleChange = event => {
+    const {
+      target: { value },
+    } = event;
+    setPersonTech(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
+        <Select
+          labelId="demo-multiple-chip-label"
+          id="demo-multiple-chip"
+          multiple
+          value={personTech}
+          onChange={handleChange}
+          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+          renderValue={selected => (
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+              {selected.map(value => (
+                <Chip key={value} label={value} />
+              ))}
+            </Box>
+          )}
+          MenuProps={MenuProps}
+        >
+          {teches.map(tech => (
+            <MenuItem
+              key={tech}
+              value={tech}
+              style={getStyles(tech, personTech, theme)}
+            >
+              {tech}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
