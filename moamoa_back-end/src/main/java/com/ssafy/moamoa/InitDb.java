30d37b224e3959f658119e3e5e2a5d2270abf57f
jiyeonyou0416 <jiyoenyou0416@naver.com>
2023-01-22 01:44:00 +0900

    :hammer: add InitDb

---
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/InitDb.java
@@ -0,0 +1,21 @@
+package com.ssafy.moamoa;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+import com.ssafy.moamoa.service.InitService;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+	private final InitService initService;
+
+	@PostConstruct
+	public void init() {
+		
+	}
+}
---
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/service/InitService.java
@@ -0,0 +1,4 @@
+package com.ssafy.moamoa.service;
+
+public class InitService {
+}
