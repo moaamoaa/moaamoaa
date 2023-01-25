56eca428c5f0ed6ffc28a72b60204e12c1a8dca4
jiyeonyou0416 <jiyoenyou0416@naver.com>
2023-01-25 11:13:50 +0900

    :hammer: update password 유효성 검사

--- moamoa_back-end/src/main/java/com/ssafy/moamoa/dto/SignForm.java
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/dto/SignForm.java
@@ -14,7 +14,7 @@ public class SignForm {
 	@Email
 	private String email;
 
-	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$",
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*?[0-9])(?=.*[\\{\\}\\[\\]\\/?.,;:|\\)*~`!^\\-_+<>@\\#$%&\\\\\\=\\(\\'\\\"]).{8,20}$",
 		message = "비밀번호는 8~20자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
 	private String password;
 
