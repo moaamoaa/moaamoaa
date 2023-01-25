e4e0991dcadf8c6f66bdc95cd1039e49542d0a5b
yeonjin <y_jin99@naver.com>
2023-01-26 08:56:10 +0900

    :bug: CORS 설정

--- moamoa_back-end/src/main/java/com/ssafy/moamoa/config/WebConfig.java
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.moamoa.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,12 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(logInterceptor).addPathPatterns("/**");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*")
+			// .allowedOrigins("http://localhost:8080")
+			.allowedMethods("GET", "POST", "PUT", "DELETE");
 	}
 }
