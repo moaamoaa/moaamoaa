00a28dda63f3c0c5b88252aab4ae937bd913e8d1
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-01-26 17:53:15 +0900

    🐛 회원가입 에러 수정 중...

--- moamoa_front-end/src/components/signUp/SignUpEmailForm.js
+++ moamoa_front-end/src/components/signUp/SignUpEmailForm.js
@@ -38,7 +38,6 @@ export default function SignUpEmailForm(props) {
         <Typography gutterBottom>모아모아에 오신 것을 환영합니다.</Typography>
         <Typography gutterBottom>이메일을 알려주세요!</Typography>
         <TextField
-          autoFocus
           margin="dense"
           id="name"
           label="Email Address"
