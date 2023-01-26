06e702850127b64aa99d32d8f66b756550d3e390
jiyeonyou0416 <jiyoenyou0416@naver.com>
2023-01-26 10:38:20 +0900

    :hammer: add area service

--- moamoa_back-end/src/main/java/com/ssafy/moamoa/controller/TechStackController.java
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/controller/TechStackController.java
@@ -1,20 +1,25 @@
 package com.ssafy.moamoa.controller;
 
-import com.ssafy.moamoa.dto.TechStackForm;
-import com.ssafy.moamoa.service.TechStackService;
-import io.swagger.annotations.ApiOperation;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.transaction.Transactional;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.ssafy.moamoa.dto.TechStackForm;
+import com.ssafy.moamoa.service.TechStackService;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
@@ -22,38 +27,37 @@ import java.util.Map;
 @RequestMapping("/techstack")
 @Transactional
 public class TechStackController {
-    private static final String SUCCESS = "SUCCESS";
-    private static final String FAIL = "FAIL";
+	private static final String SUCCESS = "SUCCESS";
+	private static final String FAIL = "FAIL";
 
-    @Autowired
-    private TechStackService techStackService;
+	@Autowired
+	private TechStackService techStackService;
 
+	@ApiOperation(value = "기술스택 검색",
+		notes = "기술스택을 검색하여 결과를 반환해줍니다.")
+	@GetMapping("/list/{techName}")
+	public ResponseEntity<?> searchTechStack(@PathVariable String techName) {
 
-    @ApiOperation(value = "기술스택 검색",
-            notes = "기술스택을 검색하여 결과를 반환해줍니다.")
-    @GetMapping("/list/{techName}")
-    public ResponseEntity<?> searchTechStack(@PathVariable String techName) {
+		List<TechStackForm> techStackFormList = techStackService.searchTechStackByName(techName);
+		return new ResponseEntity<List<TechStackForm>>(techStackFormList, HttpStatus.OK);
+	}
+	//    @ApiOperation(value = "기술스택 삭제",
+	//            notes = "")
+	//    @DeleteMapping("/{techName}")
+	//    public ResponseEntity<?> deleteTechStack(@PathVariable String techName) {
+	//
+	//
+	//        List<TechStackForm> techStackFormList = techStackService.searchTechStackByName(techName);
+	//        return new ResponseEntity<List<TechStackForm>>(techStackFormList,HttpStatus.OK);
+	//    }
 
+	@ApiOperation(value = "기술스택 등록",
+		notes = "프로필 수정을 누를 시에 사용자의 기술스택을 등록합니다.")
+	@PutMapping("/register/{userId}")
+	public ResponseEntity<?> addTechStack(@PathVariable Long userId,
+		@RequestBody List<TechStackForm> techStackFormList) {
 
-        List<TechStackForm> techStackFormList = techStackService.searchTechStackByName(techName);
-        return new ResponseEntity<List<TechStackForm>>(techStackFormList,HttpStatus.OK);
-    }
-//    @ApiOperation(value = "기술스택 삭제",
-//            notes = "")
-//    @DeleteMapping("/{techName}")
-//    public ResponseEntity<?> deleteTechStack(@PathVariable String techName) {
-//
-//
-//        List<TechStackForm> techStackFormList = techStackService.searchTechStackByName(techName);
-//        return new ResponseEntity<List<TechStackForm>>(techStackFormList,HttpStatus.OK);
-//    }
-
-    @ApiOperation(value = "기술스택 등록",
-            notes = "프로필 수정을 누를 시에 사용자의 기술스택을 등록합니다.")
-    @PutMapping("/register/{userId}")
-    public ResponseEntity<?> addTechStack(@PathVariable Long userId, @RequestBody List<TechStackForm> techStackFormList) {
-
-        String result  = techStackService.modifyUserTechStack(userId,techStackFormList);
-        return new ResponseEntity<List<TechStackForm>>(techStackFormList,HttpStatus.OK);
-    }
+		String result = techStackService.modifyUserTechStack(userId, techStackFormList);
+		return new ResponseEntity<List<TechStackForm>>(techStackFormList, HttpStatus.OK);
+	}
 }
