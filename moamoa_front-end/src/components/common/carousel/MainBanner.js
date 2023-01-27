e9417f4c59ba92b4afa0f5aec4e5e16ab7ab78c5
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-01-27 12:58:00 +0900

    ♻️ Refactor Code

--- moamoa_front-end/src/components/common/carousel/MainBanner.js
+++ moamoa_front-end/src/components/common/carousel/MainBanner.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
+
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
+
+import styled from 'styled-components';
 
 export default function MainBanner(props) {
   const { post } = props;
@@ -23,24 +25,7 @@ export default function MainBanner(props) {
         maxHeight: 'calc(100vh - 56px)',
       }}
     >
-      {/* Increase the priority of the hero background image */}
-      {
-        <img
-          style={{ display: 'none' }}
-          src={post.image}
-          alt={post.imageText}
-        />
-      }
-      <Box
-        sx={{
-          position: 'absolute',
-          top: 0,
-          bottom: 0,
-          right: 0,
-          left: 0,
-          backgroundColor: 'rgba(0,0,0,.25)',
-        }}
-      />
+      <Dim />
       <Grid container>
         <Grid item md={6}>
           <Box
@@ -61,7 +46,10 @@ export default function MainBanner(props) {
             <Typography variant="h5" color="inherit" paragraph>
               {post.description}
             </Typography>
-            <Link variant="subtitle1" href="#">
+            <Link
+              variant="subtitle1"
+              href="https://lab.ssafy.com/s08-webmobile2-sub2/S08P12B110/-/tree/develop"
+            >
               {post.linkText}
             </Link>
           </Box>
@@ -71,11 +59,11 @@ export default function MainBanner(props) {
   );
 }
 
-MainBanner.propTypes = {
-  post: PropTypes.shape({
-    description: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    imageText: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-  }).isRequired,
-};
+const Dim = styled(Box)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+`;
