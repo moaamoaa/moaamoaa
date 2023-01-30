3e73d8e546923caffd2404b052bb211951e8e643
Yeonjin Jeong <y_jin99@naver.com>
2023-01-30 23:19:18 +0900

    :art: 테스트 환경 구성

--- moamoa_back-end/src/test/java/com/ssafy/moamoa/repository/querydsl/ProjectRepositoryImplTest.java
+++ moamoa_back-end/src/test/java/com/ssafy/moamoa/repository/querydsl/ProjectRepositoryImplTest.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ssafy.moamoa.domain.ProjectCategory;
 import com.ssafy.moamoa.domain.ProjectStatus;
 import com.ssafy.moamoa.domain.dto.ProjectDto;
-import com.ssafy.moamoa.domain.dto.ProjectForm;
 import com.ssafy.moamoa.domain.dto.SearchCondition;
 import com.ssafy.moamoa.repository.ProjectRepository;
 import com.ssafy.moamoa.service.ProjectService;
@@ -34,14 +33,6 @@ class ProjectRepositoryImplTest {
 	@Test
 	public void search() throws Exception {
 		//given
-		userService.signup("ssafy@ssafy.com", "s@12341234", "김싸피");
-		ProjectForm project1 = new ProjectForm("hi", "", "", "ONLINE", "PROJECT", "2023-02-21", 2, false, 0L,
-			new Long[] {1L, 6L}, 1L);
-		ProjectForm project2 = new ProjectForm("냠", "", "", "OFFLINE", "STUDY", "2023-02-21", 2, false, 0L,
-			new Long[] {5L, 6L}, 3L);
-		projectService.creatProject(project1);
-		projectService.creatProject(project2);
-
 		List<Long> area = new ArrayList<>(Arrays.asList(1L, 2L));
 		List<Long> stack = new ArrayList<>(Arrays.asList(1L, 2L));
 		SearchCondition condition = new SearchCondition("hi", ProjectStatus.ONLINE, area, ProjectCategory.PROJECT,
