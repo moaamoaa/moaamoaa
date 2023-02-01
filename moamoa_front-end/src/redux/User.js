b3a5be91d437ca84672be767d2b4113d4227e460
pinetree-1017 <pinetree-1017@naver.com>
2023-02-01 09:07:57 +0900

    🎨 login 기능 추가

--- moamoa_front-end/src/redux/User.js
+++ moamoa_front-end/src/redux/User.js
@@ -1,34 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 // state의 초기값 (유저정보)
-const initalState = {
+const initialStateValue = {
   isLogged: false,
-  userNickname: '',
-  userPk: '',
+  userPk: null,
 };
 
 // userSlice라는 이름으로 유저 Slice 생성
-export const userSlice = createSlice({
+export const UserSlice = createSlice({
   name: 'User',
-  initalState,
+  initialState: initialStateValue,
   reducers: {
     // 로그인시 액션
     loginAccount(state, action) {
       state.isLogged = true;
-      state.userNickname = action.payload.userNickname;
       state.userPk = action.payload.userPk;
     },
     // 로그아웃시 액션
     logoutAccount(state) {
       state.isLogged = false;
-      state.userNickname = '';
-      state.userPk = '';
+      state.userPk = null;
     },
   },
 });
 
 // dispatch로 액션을 전달해 상태를 어떻게 변화시킬지 결정함.
-export const { loginAccount, logoutAccount } = userSlice.actions;
+export const { loginAccount, logoutAccount } = UserSlice.actions;
 
 // reducer
-export default userSlice.reducer;
+export default UserSlice.reducer;
