56c5f4ed5660243dd929e3e20ccf4b1e997260ac
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-02-01 09:54:27 +0900

    🎨 Update Profile UI

--- moamoa_front-end/src/components/profile/Profile.jsx
+++ moamoa_front-end/src/components/profile/Profile.jsx
@@ -1,73 +1,123 @@
 import React from 'react';
-
-import styled from 'styled-components';
-
-import Container from '@mui/material/Container';
-import Skeleton from '@mui/material/Skeleton';
-import Typography from '@mui/material/Typography';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Button from '@mui/material/Button';
+import styled from '@emotion/styled';
 
 import CardList from 'components/common/card/CardList';
-
-const userButtons = [
-  <Button key="offer">수정</Button>,
-  <Button key="chat">신청 목록</Button>,
-];
-
-const otherButtons = [
-  <Button key="offer">제안</Button>,
-  <Button key="chat">채팅</Button>,
-];
-
-const user = {
-  id: 0,
-  name: '김싸피',
-  tech: [
-    ['front-end_icons', 'javascript'],
-    ['front-end_icons', 'typescript'],
-  ],
-  link: {
-    github: 'https://github.com/LimSB-dev',
-    tistory: '',
-    velog: '',
-  },
-};
+import {
+  Button,
+  Typography,
+  Skeleton,
+  Container,
+  Box,
+  Grid,
+} from '@mui/material';
 
 export default function Profile(props) {
+  const handleEditProfile = () => {};
+
+  const user = {
+    id: 0,
+    name: '김싸피',
+    tech: [
+      ['front-end_icons', 'javascript'],
+      ['front-end_icons', 'typescript'],
+    ],
+    link: {
+      github: 'https://github.com/LimSB-dev',
+      tistory: '',
+      velog: '',
+    },
+  };
+
+  const userButtons = [
+    <ProfileButton key="offer" href="/" variant="outlined">
+      수정
+    </ProfileButton>,
+    <ProfileButton key="chat" variant="outlined">
+      신청 목록
+    </ProfileButton>,
+  ];
+
+  const otherButtons = [
+    <ProfileButton key="offer" variant="outlined">
+      제안
+    </ProfileButton>,
+    <ProfileButton key="chat" variant="outlined">
+      채팅
+    </ProfileButton>,
+  ];
+
   return (
-    <MoaProfile component="article">
-      <MoaSkeleton variant="circular" />
-      <Typography variant="h4" color="initial" fontWeight={900}>
-        {user.name}
-      </Typography>
+    <>
+      <MoaProfile
+        component="article"
+        sx={{
+          display: { xs: 'none', md: 'block' },
+          position: 'fixed',
+          width: '320px',
+        }}
+      >
+        <MoaSkeleton
+          variant="circular"
+          sx={{ width: '320px', height: '320px' }}
+        />
+        <Typography variant="h4" color="initial" fontWeight={900}>
+          {user.name}
+        </Typography>
 
-      <CardList type={'tech'} cards={user.tech}></CardList>
-      <hr />
-      <CardList type={'link'} cards={user.link}></CardList>
+        <CardList type={'tech'} cards={user.tech}></CardList>
+        <hr />
+        <CardList type={'link'} cards={user.link}></CardList>
 
-      <MoaButtons orientation="vertical" variant="contained" color="primary">
-        {user.id ? userButtons : otherButtons}
-      </MoaButtons>
-    </MoaProfile>
+        <ProfileButtonContainer>
+          {user.id ? otherButtons : userButtons}
+        </ProfileButtonContainer>
+      </MoaProfile>
+
+      <MoaProfile
+        component="article"
+        sx={{ display: { xs: 'block', md: 'none' } }}
+      >
+        <Grid container spacing={10}>
+          <Grid item xs={4}>
+            <MoaSkeleton
+              variant="circular"
+              sx={{ width: '10rem', height: '10rem' }}
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <Typography variant="h4" color="initial" fontWeight={600}>
+              {user.name}
+            </Typography>
+
+            <CardList type={'tech'} cards={user.tech}></CardList>
+            <hr />
+            <CardList type={'link'} cards={user.link}></CardList>
+          </Grid>
+        </Grid>
+
+        <ProfileButtonContainer>
+          {user.id ? otherButtons : userButtons}
+        </ProfileButtonContainer>
+      </MoaProfile>
+    </>
   );
 }
 
 const MoaProfile = styled(Container)`
-  position: relative;
-  width: 320px;
-  height: 400px;
   padding: 0;
 `;
 
 const MoaSkeleton = styled(Skeleton)`
   margin: 0 auto;
-  width: calc(160px + 8vw);
-  height: calc(160px + 8vw);
   max-width: 320px;
   max-height: 320px;
 `;
 
-const MoaButtons = styled(ButtonGroup)`
+const ProfileButtonContainer = styled(Box)`
   width: 100%;
+`;
+
+const ProfileButton = styled(Button)`
+  width: 100%;
+  margin-top: 1rem;
 `;
