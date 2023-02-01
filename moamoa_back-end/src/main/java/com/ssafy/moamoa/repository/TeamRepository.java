cc1770c499ff8d6701bb3d7defdfa69be96e7acb
Yeonjin Jeong <y_jin99@naver.com>
2023-02-01 23:39:20 +0900

    :bug: 이름 변경

--- moamoa_back-end/src/main/java/com/ssafy/moamoa/repository/TeamRepository.java
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/repository/TeamRepository.java
@@ -21,7 +21,7 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
 	@Query(value = "select team.project " +
 		"from Team team " +
 		"where team.user = :user")
-	List<Project> findByUser(User user);
+	List<Project> findProjectByUser(User user);
 
 	Optional<Team> findByRoleAndProject_Id(TeamRole role, Long projectId);
 }
