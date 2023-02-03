1b17b54a7004cf468d03cedca301f66e6d0c8e71
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-02-03 15:07:14 +0900

    ♻️ Improve Login & Sign Up User Experience

--- moamoa_front-end/src/components/common/navbar/NavbarAccount.jsx
+++ moamoa_front-end/src/components/common/navbar/NavbarAccount.jsx
@@ -18,18 +18,19 @@ import {
 
 import ChatIcon from '@mui/icons-material/Chat';
 
-import SignInDialog from 'components/signIn/SignInDialog';
+import LogInDialog from 'components/logIn/LogInDialog';
 import CheckoutDialog from 'components/signUp/CheckoutDialog';
-import FindPasswordDialog from 'components/signIn/FindPasswordDialog';
+import FindPasswordDialog from 'components/logIn/FindPasswordDialog';
 import scrollToTop from 'utils/scrollToTop';
 
 export default function NavbarAccount() {
   const [anchorElUser, setAnchorElUser] = useState(null);
-  const [signInDialog, setSignInDialog] = useState(false);
+  const [logInDialog, setLogInDialog] = useState(false);
   const [signUpDialog, setSignUpDialog] = useState(false);
   const [findPasswordDialog, setFindPasswordDialog] = useState(false);
   // 임시 로그인 확인. 나중에 지울 것
   const isLogIn = useSelector(state => state.User.isLogged);
+
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
@@ -43,7 +44,7 @@ export default function NavbarAccount() {
   };
 
   const handleClickOpen = () => {
-    setSignInDialog(true);
+    setLogInDialog(true);
   };
 
   const handleNavigate = () => {
@@ -73,38 +74,51 @@ export default function NavbarAccount() {
 
   if (isLogIn) {
     return (
-      <Box sx={{ flexGrow: 0 }}>
-        <IconButton onClick={null} sx={{ mr: 2 }}>
-          <ChatIcon />
-        </IconButton>
+      <>
+        <Box sx={{ flexGrow: 0 }}>
+          <IconButton onClick={null} sx={{ mr: 2 }}>
+            <ChatIcon />
+          </IconButton>
 
-        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-          <Avatar alt="User Profile" src="/static/images/avatar/2.jpg" />
-        </IconButton>
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <Avatar alt="User Profile" src="/static/images/avatar/2.jpg" />
+          </IconButton>
 
-        <Menu
-          sx={{ mt: '45px' }}
-          id="menu-appbar"
-          anchorEl={anchorElUser}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          open={Boolean(anchorElUser)}
-          onClose={handleCloseUserMenu}
-        >
-          {settings.map(setting => (
-            <MenuItem key={setting.text} onClick={setting.handler}>
-              <Typography textAlign="center">{setting.text}</Typography>
-            </MenuItem>
-          ))}
-        </Menu>
-      </Box>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map(setting => (
+              <MenuItem key={setting.text} onClick={setting.handler}>
+                <Typography textAlign="center">{setting.text}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+        <LogInDialog
+          logInDialog={logInDialog}
+          setLogInDialog={setLogInDialog}
+          setSignUpDialog={setSignUpDialog}
+          setFindPasswordDialog={setFindPasswordDialog}
+        ></LogInDialog>
+        <FindPasswordDialog
+          open={findPasswordDialog}
+          setFindPasswordDialog={setFindPasswordDialog}
+        />
+        <CheckoutDialog open={signUpDialog} setSignUpDialog={setSignUpDialog} />
+      </>
     );
   } else {
     return (
@@ -114,12 +128,12 @@ export default function NavbarAccount() {
             로그인
           </MoaButton>
         </Box>
-        <SignInDialog
-          signInDialog={signInDialog}
-          setSignInDialog={setSignInDialog}
+        <LogInDialog
+          logInDialog={logInDialog}
+          setLogInDialog={setLogInDialog}
           setSignUpDialog={setSignUpDialog}
           setFindPasswordDialog={setFindPasswordDialog}
-        ></SignInDialog>
+        ></LogInDialog>
         <FindPasswordDialog
           open={findPasswordDialog}
           setFindPasswordDialog={setFindPasswordDialog}
