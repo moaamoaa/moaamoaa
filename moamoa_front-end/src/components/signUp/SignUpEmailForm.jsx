4dfc177470262294ade29123e58ce0c817935e79
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-02-03 15:09:02 +0900

    ♻️ Improve Login & Sign Up User Experience

--- moamoa_front-end/src/components/signUp/SignUpEmailForm.jsx
+++ moamoa_front-end/src/components/signUp/SignUpEmailForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import customAxios from 'utils/axios';
 
 import {
   TextField,
@@ -10,36 +10,32 @@ import {
 } from '@mui/material/';
 
 export default function SignUpEmailForm(props) {
-  // 기본 url
-  const baseUrl = 'http://localhost:8080';
-  // input에 작성하는 이메일 정보
   const [userEmail, setUserEmail] = useState(props.email);
-  const [emailError, setEmailError] = useState(false);
-  const changeEmailHandler = e => {
+  const [emailMessage, setEmailMessage] = useState('');
+
+  const handleEmail = e => {
     setUserEmail(e.target.value);
-    setEmailError(false);
   };
 
-  // email 중복검사 혹은 이메일 형식이 다를때 보여줄 오류 메시지
+  const getCode = () => {
+    // 로딩창 보여주기
 
-  const getCode = async e => {
-    try {
-      // 로딩창 보여주기
-      const response = await axios.get(
-        `${baseUrl}/users/email?email=${userEmail}`,
-      );
-      // 이메일 중복 검사에 걸리지 않았다면, stageHandler를 다음 단계로 넘어가게함
-      props.setActiveStep(1);
-      console.log(response.data);
+    customAxios.basicAxios
+      .get(`/users/email?email=${userEmail}`)
+      .then(response => {
+        props.setActiveStep(1);
 
-      // 부모 컴포넌트에 이메일과 인증코드를 props로 넘겨줌
-      props.emailHandler(userEmail);
-      props.codeHandler(response.data);
-    } catch (error) {
-      // 이메일 중복검사에서 중복된 이메일이면 409 에러와 함께 백에서 메시지 넘어옴
-      console.log(error);
-      setEmailError(true);
-    }
+        props.handleEmail(userEmail);
+        props.handleCode(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        const errorStatus = error.response.data.status;
+
+        if (errorStatus === 409) setEmailMessage('이미 가입된 이메일 입니다.');
+        if (errorStatus === 500)
+          setEmailMessage('잘못된 형식의 이메일 입니다.');
+      });
   };
 
   if (props.type === 'signUpEmail') {
@@ -56,8 +52,8 @@ export default function SignUpEmailForm(props) {
           variant="outlined"
           required={true}
           value={userEmail}
-          onChange={changeEmailHandler}
-          helperText={emailError ? '다른 이메일을 작성해주세요' : ''}
+          onChange={handleEmail}
+          helperText={emailMessage}
         />
         <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
           <Button variant="contained" sx={{ mt: 3, ml: 1 }} onClick={getCode}>
