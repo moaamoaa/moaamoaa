6c98ed62a411103091588fa94a247b1139dc212d
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-02-05 13:33:17 +0900

    🎨 Update Store Format

--- moamoa_front-end/src/redux/store.js
+++ moamoa_front-end/src/redux/store.js
@@ -1,30 +1,30 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
-import { persistReducer } from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
 
 import userReducer from 'redux/user';
 import searchReducer from 'redux/search';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user', 'search'],
+};
 
 const reducers = combineReducers({
   user: userReducer,
   search: searchReducer,
 });
 
-// persisit 설정
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['user', 'search'],
-};
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
   reducer: persistedReducer,
-  // non-serializable value was detected 라고 뜨는 에러 처리를 위해 사용
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+  }),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export default { store, persistor };
