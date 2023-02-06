f64904fa65b42d6827cff52ca8686e9c222d1f75
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-02-06 22:10:17 +0900

    🎨 Update Side Project

--- moamoa_front-end/src/components/profile/SideProject.jsx
+++ moamoa_front-end/src/components/profile/SideProject.jsx
@@ -2,32 +2,60 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 
-import { Typography, Grid } from '@mui/material';
+import { Typography, Grid, Container } from '@mui/material';
 import LongMenu from './LongMenu';
 
 export default function SideProject() {
   return (
-    <Grid container spacing={5}>
-      <Grid item xs={1} md={1}>
-        <SideProjectYear item>{}</SideProjectYear>
-      </Grid>
-      <Grid item xs={10} md={11}>
-        <SideProjectTitle variant="body1" color="initial">
-          {}
-        </SideProjectTitle>
-        <Grid container>
-          {/* {techStacks.map((techStack, idx) => (
+    <>
+      <ContentTitle color="initial">주요 프로젝트</ContentTitle>
+      <MoaContainer spacing={0}>
+        <Grid container spacing={5}>
+          <Grid item xs={1} md={1}>
+            <SideProjectYear>{}</SideProjectYear>
+          </Grid>
+          <Grid item xs={10} md={11}>
+            <SideProjectTitle variant="body1" color="initial">
+              {}
+            </SideProjectTitle>
+            <Grid container>
+              {/* {techStacks.map((techStack, idx) => (
             <TechStackGrid key={idx}>{techStack}</TechStackGrid>
           ))} */}
+            </Grid>
+            <SideProjectContext>{}</SideProjectContext>
+          </Grid>
+          <Grid item xs={1}>
+            <ProfileLongMenu></ProfileLongMenu>
+          </Grid>
         </Grid>
-        <SideProjectContext>{}</SideProjectContext>
-      </Grid>
-      <Grid item xs={1}>
-        <ProfileLongMenu></ProfileLongMenu>
-      </Grid>
-    </Grid>
+      </MoaContainer>
+    </>
   );
 }
+
+const ContentTitle = styled(Typography)`
+  font-weight: bolder;
+  font-size: 1.5em;
+
+  margin-bottom: 0.5rem;
+`;
+
+const MoaContainer = styled(Container)`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  width: 100%;
+  min-height: 8rem;
+
+  margin-bottom: 4rem;
+  padding: 1rem;
+
+  border: 1px solid black;
+  border-radius: 5px;
+`;
 
 const SideProjectYear = styled(Typography)`
   font-weight: bolder;
