b7389c6f69edda6f4efcae8d7f59b021a8b07b39
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-02-08 00:50:31 +0900

    🎨 Update Navbar UI

--- moamoa_front-end/src/components/common/navbar/Navbar.jsx
+++ moamoa_front-end/src/components/common/navbar/Navbar.jsx
@@ -11,6 +11,8 @@ import {
 
 import NavbarLogo from 'components/common/navbar/NavbarLogo';
 import NavbarAccount from 'components/common/navbar/NavbarAccount';
+import NavbarMenu from 'components/common/navbar/NavbarMenu';
+
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -26,18 +28,49 @@ export default function ResponsiveAppBar() {
     },
   ];
   return (
-    <AppBar position="fixed">
+    <AppBar position="fixed" sx={{ backgroundColor: '#blue' }}>
       <Container fixed>
         <MoaAccordion sx={{ border: 0 }}>
           <Toolbar disableGutters>
-            <NavbarLogo></NavbarLogo>
-
-            <NavbarAccount></NavbarAccount>
+            <Grid
+              container
+              spacing={0}
+              alignItems="center"
+              sx={{ display: { xs: 'flex', md: 'none' } }}
+            >
+              <Grid item xs={4}>
+                <NavbarMenu></NavbarMenu>
+              </Grid>
+              <Grid item xs={4}>
+                <NavbarLogo></NavbarLogo>
+              </Grid>
+              <Grid item xs={4} sx={{ display: 'flex' }} justifyContent="end">
+                <NavbarAccount></NavbarAccount>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              spacing={0}
+              alignItems="center"
+              sx={{ display: { xs: 'none', md: 'flex' } }}
+            >
+              <Grid item container xs={4} alignItems="center">
+                <Grid item xs={5}>
+                  <NavbarLogo></NavbarLogo>
+                </Grid>
+                <Grid item xs={7}>
+                  <NavbarMenu></NavbarMenu>
+                </Grid>
+              </Grid>
+              <Grid item xs={8} sx={{ display: 'flex' }} justifyContent="end">
+                <NavbarAccount></NavbarAccount>
+              </Grid>
+            </Grid>
           </Toolbar>
           <AccordionDetails sx={{ padding: '0' }}>
             <Grid>
               {pages.map((page, idx) => (
-                <ListItem key={idx} sx={{ paddingLeft: '0' }}>
+                <ListItem key={idx} sx={{ padding: '0', py: '1rem' }}>
                   <Typography textAlign="center">
                     <MoaLink to={page.link}>{page.text}</MoaLink>
                   </Typography>
