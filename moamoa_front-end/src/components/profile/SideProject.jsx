0071336dcb268cb53d819420098b611c65338554
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-02-08 00:46:55 +0900

    🎨 Update Side Project

--- moamoa_front-end/src/components/profile/SideProject.jsx
+++ moamoa_front-end/src/components/profile/SideProject.jsx
@@ -4,31 +4,75 @@ import styled from '@emotion/styled';
 
 import { Typography, Grid, Container } from '@mui/material';
 import LongMenu from './LongMenu';
+import { useSelector } from 'react-redux';
 
 export default function SideProject() {
+  const SideProjects = useSelector(state => state.profile.SideProjects);
+  const techStacks = useSelector(state => state.profile.techStacks);
+
   return (
     <>
       <ContentTitle color="initial">주요 프로젝트</ContentTitle>
-      <MoaContainer spacing={0}>
-        <Grid container spacing={5}>
-          <Grid item xs={1} md={1}>
-            <SideProjectYear>{}</SideProjectYear>
-          </Grid>
-          <Grid item xs={10} md={11}>
-            <SideProjectTitle variant="body1" color="initial">
-              {}
-            </SideProjectTitle>
-            <Grid container>
-              {/* {techStacks.map((techStack, idx) => (
-            <TechStackGrid key={idx}>{techStack}</TechStackGrid>
-          ))} */}
+      <MoaContainer container>
+        {SideProjects ? (
+          SideProjects.map((SideProject, idx) => {
+            <Grid key={idx} item container alignItems="start">
+              <Grid item xs={1} md={1}>
+                <SideProjectYear>{SideProject.year}</SideProjectYear>
+              </Grid>
+              <Grid item xs={10} md={11}>
+                <SideProjectTitle variant="body1" color="initial">
+                  {SideProject.title}
+                </SideProjectTitle>
+                <Grid container>
+                  {techStacks ? (
+                    techStacks.map((techStack, idx) => (
+                      <TechStackGrid key={idx}>{techStack}</TechStackGrid>
+                    ))
+                  ) : (
+                    <>
+                      <TechStackGrid>HTML</TechStackGrid>
+                      <TechStackGrid>CSS</TechStackGrid>
+                      <TechStackGrid>JavaScript</TechStackGrid>
+                    </>
+                  )}
+                </Grid>
+                <SideProjectContext>프로젝트 소개</SideProjectContext>
+              </Grid>
+              <Grid item xs={1} justifyContent="end" sx={{ display: 'flex' }}>
+                <ProfileLongMenu></ProfileLongMenu>
+              </Grid>
+            </Grid>;
+          })
+        ) : (
+          <Grid item container alignItems="start">
+            <Grid item xs={1}>
+              <SideProjectYear>2023</SideProjectYear>
             </Grid>
-            <SideProjectContext>{}</SideProjectContext>
+            <Grid item xs={10}>
+              <SideProjectTitle variant="body1" color="initial">
+                프로젝트명
+              </SideProjectTitle>
+              <Grid container>
+                {techStacks ? (
+                  techStacks.map((techStack, idx) => (
+                    <TechStackGrid key={idx}>{techStack}</TechStackGrid>
+                  ))
+                ) : (
+                  <>
+                    <TechStackGrid>HTML</TechStackGrid>
+                    <TechStackGrid>CSS</TechStackGrid>
+                    <TechStackGrid>JavaScript</TechStackGrid>
+                  </>
+                )}
+              </Grid>
+              <SideProjectContext>프로젝트 소개</SideProjectContext>
+            </Grid>
+            <Grid item xs={1} justifyContent="end" sx={{ display: 'flex' }}>
+              <ProfileLongMenu></ProfileLongMenu>
+            </Grid>
           </Grid>
-          <Grid item xs={1}>
-            <ProfileLongMenu></ProfileLongMenu>
-          </Grid>
-        </Grid>
+        )}
       </MoaContainer>
     </>
   );
@@ -41,7 +85,7 @@ const ContentTitle = styled(Typography)`
   margin-bottom: 0.5rem;
 `;
 
-const MoaContainer = styled(Container)`
+const MoaContainer = styled(Grid)`
   position: relative;
   display: flex;
   justify-content: space-between;
