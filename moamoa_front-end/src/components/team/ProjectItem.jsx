6a44d68c7a8e76ae60fe4edc9e58dccb419a62bc
jyuha <jyuha759@gmail.com>
2023-02-08 17:58:04 +0900

    :art: Need to use redux

--- moamoa_front-end/src/components/team/ProjectItem.jsx
+++ moamoa_front-end/src/components/team/ProjectItem.jsx
@@ -7,11 +7,23 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProjectItem(props) {
+  const navigate = useNavigate();
+  const goToDetail = () => {
+    // 주소가 정확히 어떻게 되지?
+    navigate(`/projects/detail?projectId=${props.project.projectId}`);
+  };
+
   if (props.type === 'project') {
     return (
-      <ListItemButton sx={{ pl: 4 }} alignItems="flex-start">
+      // 버튼에 온클릭 넣어서 팀 페이지 open으로 가게 (파라미터 가진)
+      <ListItemButton
+        onClick={goToDetail}
+        sx={{ pl: 4 }}
+        alignItems="flex-start"
+      >
         <ListItemAvatar>
           <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
         </ListItemAvatar>
@@ -35,7 +47,11 @@ export default function ProjectItem(props) {
     );
   } else if (props.type === 'study') {
     return (
-      <ListItemButton sx={{ pl: 4 }} alignItems="flex-start">
+      <ListItemButton
+        onClick={goToDetail}
+        sx={{ pl: 4 }}
+        alignItems="flex-start"
+      >
         <ListItemAvatar>
           <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
         </ListItemAvatar>
