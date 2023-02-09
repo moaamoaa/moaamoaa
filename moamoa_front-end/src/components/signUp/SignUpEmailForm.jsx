98c885e39443a106f2b07c9aebe22368cfb7d09e
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-02-10 01:32:21 +0900

    🎨 Update Signup

--- moamoa_front-end/src/components/signUp/SignUpEmailForm.jsx
+++ moamoa_front-end/src/components/signUp/SignUpEmailForm.jsx
@@ -7,11 +7,14 @@ import {
   Typography,
   Button,
   Box,
+  Backdrop,
+  CircularProgress,
 } from '@mui/material/';
 
 export default function SignUpEmailForm(props) {
   const [userEmail, setUserEmail] = useState(props.email);
   const [emailMessage, setEmailMessage] = useState('');
+  const [open, setOpen] = useState(false);
 
   const handleEmail = e => {
     setUserEmail(e.target.value);
@@ -20,10 +23,13 @@ export default function SignUpEmailForm(props) {
   const getCode = () => {
     // 로딩창 보여주기
 
+    setOpen(true);
+
     customAxios.basicAxios
       .get(`/users/email?email=${userEmail}`)
       .then(response => {
         props.setActiveStep(1);
+        setOpen(false);
 
         props.handleEmail(userEmail);
         props.handleCode(response.data);
@@ -41,6 +47,13 @@ export default function SignUpEmailForm(props) {
   if (props.type === 'signUpEmail') {
     return (
       <DialogContent dividers>
+        <Backdrop
+          sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }}
+          open={open}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+
         <Typography gutterBottom>모아모아에 오신 것을 환영합니다.</Typography>
         <Typography gutterBottom>이메일을 알려주세요!</Typography>
         <TextField
