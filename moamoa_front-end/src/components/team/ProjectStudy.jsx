93f6cc989c0a4a77318876195fdcc3fa1042077b
jyuha <jyuha759@gmail.com>
2023-02-10 14:47:59 +0900

    :art: Display team members in TeamDetailPage

--- moamoa_front-end/src/components/team/ProjectStudy.jsx
+++ moamoa_front-end/src/components/team/ProjectStudy.jsx
@@ -4,7 +4,6 @@ import CustomAxios from 'utils/axios';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 // import ListItem from '@mui/material/ListItem';
