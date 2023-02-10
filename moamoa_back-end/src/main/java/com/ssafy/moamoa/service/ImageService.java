5f166ce542dfa100e4822386303be8833d673c73
smartpodo@kakao.com <smartpodo@kakao.com>
2023-02-10 09:21:48 +0900

    :sparkles: Get Random Image Profile & Project

--- moamoa_back-end/src/main/java/com/ssafy/moamoa/service/ImageService.java
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/service/ImageService.java
@@ -1,13 +1,13 @@
 package com.ssafy.moamoa.service;
 
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,25 +15,25 @@ import java.util.Random;
 
 public class ImageService {
 
-    String defaultPath = "https://s3.ap-northeast-2.amazonaws.com/ssafy.moamoa.image.bucket/default/";
-    ArrayList<String> defaultProfileImages =new ArrayList<>(List.of("blue_cat.png","blue_dog.png","yellow_cat.png","yellow_dog.png"));
+	String defaultPath = "https://s3.ap-northeast-2.amazonaws.com/ssafy.moamoa.image.bucket/default/";
+	ArrayList<String> defaultProfileImages =new ArrayList<>(List.of("blue_cat.png","blue_dog.png","yellow_cat.png","yellow_dog.png"));
 
-    ArrayList<String> defaultProjectImages = new ArrayList<>(List.of("engineering_team_blue.png","engineering_team_yellow.png"));
-    Random rand;
+	ArrayList<String> defaultProjectImages = new ArrayList<>(List.of("engineering_team_blue.png","engineering_team_yellow.png"));
+	Random rand;
 
-    public String getRandomDefaultProfileImage()
-    {
-        rand = new Random();
-        int index = rand.nextInt(defaultProfileImages.size());
+	public String getRandomDefaultProfileImage()
+	{
+		rand = new Random();
+		int index = rand.nextInt(defaultProfileImages.size());
 
-        return defaultPath+defaultProfileImages.get(index);
-    }
+		return defaultPath+defaultProfileImages.get(index);
+	}
 
-    public String getRandomDefaultProjectImage()
-    {
-        rand = new Random();
-        int index = rand.nextInt(defaultProjectImages.size());
+	public String getRandomDefaultProjectImage()
+	{
+		rand = new Random();
+		int index = rand.nextInt(defaultProjectImages.size());
 
-        return defaultPath+defaultProjectImages.get(index);
-    }
+		return defaultPath+defaultProjectImages.get(index);
+	}
 }
