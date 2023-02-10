26b0511ccd50f194aa583fcc78170da0a2e162b6
jyuha <jyuha759@gmail.com>
2023-02-10 13:47:27 +0900

    :art: Sidebar Size

--- moamoa_front-end/src/components/team/ProjectStudy.jsx
+++ moamoa_front-end/src/components/team/ProjectStudy.jsx
@@ -6,22 +6,22 @@ import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
+// import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
 import Collapse from '@mui/material/Collapse';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+// import MailIcon from '@mui/icons-material/Mail';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import SendIcon from '@mui/icons-material/Send';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import StarBorder from '@mui/icons-material/StarBorder';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
+// import SendIcon from '@mui/icons-material/Send';
+// import DraftsIcon from '@mui/icons-material/Drafts';
+// import StarBorder from '@mui/icons-material/StarBorder';
+// import Avatar from '@mui/material/Avatar';
+// import Typography from '@mui/material/Typography';
+// import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ProjectList from 'components/team/ProjectList';
 
 export default function ApplyOffer() {
@@ -67,6 +67,7 @@ export default function ApplyOffer() {
   const [state, setState] = React.useState({
     right: false,
   });
+
   // 리스트가 열리는
   const [open, setOpen] = React.useState(true);
   // 리스트가 열리는
@@ -87,7 +88,7 @@ export default function ApplyOffer() {
 
   const list = anchor => (
     <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 500 }}
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 360 }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
@@ -142,7 +143,9 @@ export default function ApplyOffer() {
     <div>
       {['right'].map(anchor => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>팀관리아이콘</Button>
+          <Button onClick={toggleDrawer(anchor, true)}>
+            팀관리아이콘을 네브로 빼버려?
+          </Button>
           <Drawer
             anchor={anchor}
             open={state[anchor]}
