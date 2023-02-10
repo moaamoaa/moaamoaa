fc74b53a5bf1c8f7e200dbd0c5cd3b946eea9180
jyuha <jyuha759@gmail.com>
2023-02-10 17:53:39 +0900

    :refactoring: Refactore Code

--- moamoa_front-end/src/components/common/card/TeamMemberSearchList.js
+++ moamoa_front-end/src/components/common/card/TeamMemberSearchList.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-// import customAxios from 'utils/axios';
+import React from 'react';
 import CardList from 'components/common/card/CardList';
 
 export default function TeamMemberSearchList(props) {
