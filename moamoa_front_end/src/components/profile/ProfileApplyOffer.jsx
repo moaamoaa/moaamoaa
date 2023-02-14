9902bf855dc4caf12a962820572652cce831a1bc
jyuha <jyuha759@gmail.com>
2023-02-15 01:13:46 +0900

    :bug: Correction of Sidebar Closing Phenomenon

--- moamoa_front_end/src/components/profile/ProfileApplyOffer.jsx
+++ moamoa_front_end/src/components/profile/ProfileApplyOffer.jsx
@@ -97,8 +97,8 @@ export default function ProfileApplyOffer() {
     <Box
       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 360 }}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
+      // onClick={toggleDrawer(anchor, false)}
+      // onKeyDown={toggleDrawer(anchor, false)}
     >
       <List
         sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
@@ -120,7 +120,11 @@ export default function ProfileApplyOffer() {
         <Collapse in={openProjects} timeout="auto" unmountOnExit>
           <Divider />
           <List component="div" disablePadding>
-            <ListItemButton sx={{ pl: 4 }} alignItems="flex-start">
+            <ListItemButton
+              onClick={toggleDrawer(anchor, !state[anchor])}
+              sx={{ pl: 4 }}
+              alignItems="flex-start"
+            >
               {/* 받은 제안 리스트 컴포넌트 */}
               <ProfileList offers={offers} type={'offer'}></ProfileList>
             </ListItemButton>
@@ -136,7 +140,11 @@ export default function ProfileApplyOffer() {
         <Collapse in={openStudies} timeout="auto" unmountOnExit>
           <Divider />
           <List component="div" disablePadding>
-            <ListItemButton sx={{ pl: 4 }} alignItems="flex-start">
+            <ListItemButton
+              onClick={toggleDrawer(anchor, !state[anchor])}
+              sx={{ pl: 4 }}
+              alignItems="flex-start"
+            >
               {/* 보낸 지원 리스트 컴포넌트 */}
               <ProfileList applies={applies} type={'apply'}></ProfileList>
             </ListItemButton>
@@ -165,7 +173,7 @@ export default function ProfileApplyOffer() {
             anchor={anchor}
             open={state[anchor]}
             onClose={toggleDrawer(anchor, false)}
-            onClick={toggleDrawer(anchor, !state[anchor])}
+            // onClick={toggleDrawer(anchor, !state[anchor])}
           >
             {list(anchor)}
           </Drawer>
