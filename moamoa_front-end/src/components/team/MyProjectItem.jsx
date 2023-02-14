8eaa622e91b6e340cc4ad8f84c7d28734c110e4c
jyuha <jyuha759@gmail.com>
2023-02-14 16:23:27 +0900

    :art: 제안 보내기 성공

--- moamoa_front-end/src/components/team/MyProjectItem.jsx
+++ moamoa_front-end/src/components/team/MyProjectItem.jsx
@@ -17,21 +17,25 @@ import customAxios from 'utils/axios';
 export default function MyProjectItem(props) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const memberId = useSelector(state => state.member.memberId);
   const goToDetail = () => {
     dispatch(handleOpenTeamDetail({ projectId: props.projectstudy.projectId })); // 오픈 했을 때, 값을 바꿔주고 그걸 디테일로 보내
     // 팀 관리에서 팀을 눌렀을 때 이동할 프론트 디테일 페이지 주소
     navigate(`/TeamDetailPage/?projectId=${props.projectstudy.projectId}`);
   };
-  // 팀에서 개인에게 제안 보내기 ( *일단 사이드바를 열게 됨 -> 팀원 구하기 페이지에서 혹은 프로필 페이지에서 가능 )
+  // 팀에서 개인에게 제안 보내기
   const handleOffer = () => {
-    console.log(userPk);
     console.log(props.projectstudy.projectId);
+    // 카드의 props.type이 member이면 props.card.id가 userId임
     customAxios
       .authAxios({
         method: 'POST',
         url: '/offer',
-        // userId 는 제안을 받을 사람 ID : 어떻게 얻어오지?
-        data: { projectId: props.projectstudy.projectId, userId: userPk },
+        // userId 는 제안을 받을 사람 ID : 카드 주인의 ID
+        data: {
+          projectId: props.projectstudy.projectId,
+          userId: memberId,
+        },
       })
       .then(response => {
         dispatch(
