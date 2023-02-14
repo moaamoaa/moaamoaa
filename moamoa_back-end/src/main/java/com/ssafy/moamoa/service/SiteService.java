fa2b1b4fd0ea1708da7cd24f120b16358913c6c7
smartpodo@kakao.com <smartpodo@kakao.com>
2023-02-14 10:02:19 +0900

    :memo: Add Image to SiteForm DTO
    Site 로고를 보내주지 않았던 것을 이제 DTO에 logo 안에 보내 주는것으로 변경하였습니다.

--- moamoa_back-end/src/main/java/com/ssafy/moamoa/service/SiteService.java
+++ moamoa_back-end/src/main/java/com/ssafy/moamoa/service/SiteService.java
@@ -1,23 +1,21 @@
 package com.ssafy.moamoa.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.ssafy.moamoa.domain.dto.SiteForm;
 import com.ssafy.moamoa.domain.entity.Profile;
 import com.ssafy.moamoa.domain.entity.ProfileSite;
+import com.ssafy.moamoa.domain.entity.Site;
 import com.ssafy.moamoa.repository.ProfileRepository;
 import com.ssafy.moamoa.repository.ProfileSiteRepository;
 import com.ssafy.moamoa.repository.SiteRepository;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = false)
@@ -34,40 +32,46 @@ public class SiteService {
 
     private final ProfileSiteRepository profileSiteRepository;
 
-    public List<SiteForm> getProfileSites(Long profileId)
-    {
+    public List<SiteForm> getProfileSites(Long profileId) {
         List<ProfileSite> profileSiteList = profileSiteRepository.getProfileSitesByIdAsc(profileId);
         List<SiteForm> siteFormList = new ArrayList<>();
-        for(ProfileSite profileSite : profileSiteList)
-        {
-        SiteForm siteForm = SiteForm.builder()
-                .name(profileSite.getSite().getName())
-                .link(profileSite.getLink()).build();
+        for (ProfileSite profileSite : profileSiteList) {
+            SiteForm siteForm = SiteForm.builder()
+                    .name(profileSite.getSite().getName())
+                    .logo(profileSite.getSite().getLogo())
+                    .link(profileSite.getLink()).build();
 
-        siteFormList.add(siteForm);
+            siteFormList.add(siteForm);
         }
         return siteFormList;
     }
 
-    public List<SiteForm> modifyProfileSite(Long profileId, List<SiteForm> siteFormList)
-    {
+    public List<SiteForm> modifyProfileSite(Long profileId, List<SiteForm> siteFormList) {
         Profile profile = profileRepository.getProfileById(profileId);
-
+        List<SiteForm> returnList = new ArrayList<>();
         Long count = profileSiteRepository.deleteProfileSiteById(profileId);
 
-        for(SiteForm siteForm : siteFormList)
-        {
+        for (SiteForm siteForm : siteFormList) {
+            Site site = siteRepository.getSiteByName(siteForm.getName());
             ProfileSite profileSite = ProfileSite.builder()
                     .profile(profile)
-                    .site(siteRepository.getSiteByName(siteForm.getName()))
+                    .site(site)
                     .link(siteForm.getLink())
                     .build();
 
-             profileSiteRepository.save(profileSite);
+            SiteForm result = SiteForm.builder()
+                    .name(siteForm.getName())
+                    .link(siteForm.getLink())
+                    .logo(site.getLogo()).build();
 
+            profileSiteRepository.save(profileSite);
+            returnList.add(result);
         }
 
-        return siteFormList;
+        // Return
+
+
+        return returnList;
     }
 
 }
